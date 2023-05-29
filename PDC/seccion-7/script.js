2920fbcde6e471698cdb2cc6b5e3e71fa2395dff
Aldair <djaldair14@hotmail.com>
2023-05-28 20:17:53 -0500

    third commit

--- PDC/seccion-7/script.js
+++ PDC/seccion-7/script.js
@@ -21,3 +21,8 @@ function validateForm() {
     alert("Correo electrónico enviado correctamente.");
   }
 }
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
